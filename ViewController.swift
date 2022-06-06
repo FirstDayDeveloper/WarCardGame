59163dbcd3fd7eebaea8775ebfdc5d112e8451d4
Kevin Nyangena <nyangena.kevin@students.jkuat.ac.ke>
2022-06-06 10:51:22 +0100

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Kevin Nyangena on 11/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+        
+    }
+
+    @IBAction func dealTapped(_ sender: Any) {
+        
+        let leftNumber = Int.random(in: 2...14)
+        
+        let rightNumber = Int.random(in: 2...14)
+        
+        leftImageView.image = UIImage(named: "card\(leftNumber)")
+        
+        rightImageView.image = UIImage(named: "card\(rightNumber)")
+    }
+    
+    
+}
+
